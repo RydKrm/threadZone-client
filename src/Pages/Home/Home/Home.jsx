d96538551d9261cd429abd10f714d131b7eef6c8
Abu-Sahad <abusahad603@gmail.com>
2023-07-25 11:04:01 +0600

    git colaborate

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
             <h1 className="text-3xl text-center font-bold underline">
                 Hello world!
             </h1>
+            <h2>git hub merge collaborate</h2>
         </div>
     );
 };
