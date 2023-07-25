93624607553bf2db875e4dc7db84045508f4c20e
RydKrm <alriyadkarim@gmail.com>
2023-07-25 10:49:54 +0600

    change again

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,7 +4,8 @@ const Home = () => {
             <h1 className="text-3xl text-center font-bold underline">
                 Hello world! this is home page
             </h1>
-            <h2>This is changed</h2>
+            <h2>This is changed</h2> 
+            <h3>Checking the change</h3>
         </div>
     );
 };
