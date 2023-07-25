d2777b725e92b20b951f550a71a8e9e3fc29a6b4
RydKrm <124508907+RydKrm@users.noreply.github.com>
2023-07-25 11:08:26 +0600

    Update Home.jsx

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
             <h2>This is changed</h2> 
             <h3>Checking the change </h3>
             <h3> Changed make again </h3>
+            <h3> Change making last time</h3>
         </div>
     );
 };
