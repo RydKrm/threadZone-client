55d6c04fcc676c6831bece93b5756628518d1669
RydKrm <alriyadkarim@gmail.com>
2023-07-25 19:36:45 +0600

    tailwind config change

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 const Home = () => {
+    const [data,setData] = useState();
+    //const find = 'logging';
     return (
-<div>
-      <h2> Change code for resolved </h2>
-      </div>
+      <div>
+        <h2> Change code for resolved </h2>
+        <h2 className="font-xl color-red-5000">snvcisndci</h2>
+        <img src="../../../assets/Ecommerce image/gadgets-3.jpg" alt="" />
+      </div> 
     );
 };
 
