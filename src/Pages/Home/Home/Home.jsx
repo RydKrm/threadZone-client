cdb36d2d8bdef4ae88c3bd8115f1a356839f0faa
Abu-Sahad <abusahad603@gmail.com>
2023-07-25 12:18:15 +0600

    home updated 2

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
     return (
        <div>
       <h1>hey this is my code </h1>
+      <h1>hey this is my code1 </h1>
+      <h1>hey this is my code2 </h1>
       </div>
     );
 };
