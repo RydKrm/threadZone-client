3194676ba3f6feb5e36bfc33e366d083f1b049aa
Md. Abu Sahad <82924820+Abu-Sahad@users.noreply.github.com>
2023-07-25 10:12:18 +0600

    Update Home.jsx

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1 className="text-3xl text-center font-bold underline">
-                Hello world!
+                Hello world! this is home page
             </h1>
         </div>
     );
