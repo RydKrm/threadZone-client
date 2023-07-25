653b73bc3f65d7fb00a92fc37e7a969d83e9cf6d
RydKrm <124508907+RydKrm@users.noreply.github.com>
2023-07-25 12:03:04 +0600

    Update Home.jsx

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,8 +2,6 @@ const Home = () => {
     return (
         <div>
             <h1>hellow word</h1>
-            <h2>This is changed</h2> 
-            <h3>Checking the change </h3>
             <h3> Changed make again </h3>
             <h3> Change making last time</h3>
             <h3>A new branch created</h3>
