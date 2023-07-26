bb7fd357ebf40bde2503515f2b2e8c52c5094146
RydKrm <alriyadkarim@gmail.com>
2023-07-26 10:08:36 +0600

    product page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      dark: "#363538",
+      lightDark: "#8D8C8A",
+      lightWhite: "#F6F6F6",
+      darkBlue: "#408697",
+      lightBlue: "#52D6F4",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+};
 
