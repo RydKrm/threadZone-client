32867645b788dc03a7dfc6c05c112581aed8bc71
Abu-Sahad <abusahad603@gmail.com>
2023-07-26 13:20:12 +0600

    footer added

---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer className='mb-2'>
+            <div className="footer p-10 bg-base-200 text-base-content ">
+                <div>
+
+                    <p>ThreadZone Emart Ltd.<br />Providing reliable tech since 2023</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className="footer footer-center p-4 bg-base-300 text-base-content">
+                <div>
+                    <p>Copyright © {currentYear} - All right reserved by ThreadZone Emart Ltd</p>
+                </div>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Footer;
