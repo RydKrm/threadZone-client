445b04647bb32bf5a578b927a5aca6fd44eea08d
Abu-Sahad <abusahad603@gmail.com>
2023-08-09 19:52:59 +0600

    update admin shop approval

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -76,14 +76,6 @@ const Dashboard = () => {
                                         <li><Link to='/dashboard/addProduct'>Add Product</Link></li>
                                         <li><Link to='/dashboard/addShop'>Create Shop</Link></li>
                                         <li><Link to='/dashboard/shopRequest'>Admin Sop Approval</Link></li>
-                                        <li><Link to='/dashboard/returnList'>My Returns</Link></li>
-                                        <li><Link to='/dashboard/orderList'>My Orders</Link></li>
-                                        <li><Link to='/dashboard/previousOrderList'>My Previous Orders</Link></li>
-                                        <li><Link to='/dashboard/reviewList'>My Review</Link></li>
-                                        <li><Link to='/dashboard/addProduct'>Add Product</Link></li>
-                                        <li><Link to='/dashboard/editCategory'>Admin Edit Category</Link></li>
-                                        <li><Link to='/dashboard/editCategory'>Admin Edit Category</Link></li>
-                                        <li><Link to='/dashboard/shopRequest'>Admin Shop Approval</Link></li>
                                         <li><Link to='/dashboard/editCategory'>Admin Edit Category</Link></li>
                                         <li className="mt-5">
                                             <span className="text-red-600">Payment Method</span>
